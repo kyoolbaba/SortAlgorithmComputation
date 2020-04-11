bdedbb46866f1e60300336e505ad8cb3ba2bcd45
kyoolbaba <kyoolbaba@gmail.com>
2020-04-11 21:11:51 +0530

    Adding feature to compute (a%b+c)

--- sortAlgoComputation.sh
+++ sortAlgoComputation.sh
@@ -3,5 +3,6 @@ echo Enter the three numbers separated by a space
 read A B C 
 computation1=$((A+B+C))
 computation2=$((A*B+C))
-computation4=$((C+A/B))
+computation3=$((C+A/B))
+computation4=$((A%B+C))
 
