544d71b4378409d71210a53e55e2180c284c5e6a
kyoolbaba <kyoolbaba@gmail.com>
2020-04-11 21:47:59 +0530

    Adding feature to save values to array and sorting it in ascedning order

--- sortAlgoComputation.sh
+++ sortAlgoComputation.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 echo Enter the three numbers separated by a space
 read A B C 
+declare -a arr
 computation["A+B+C"]=$((A+B+C))
 computation["A*B+C"]=$((A*B+C))
 computation["C+A/B"]=$((C+A/B))
 computation["A%B+C"]=$((A%B+C))
-
+for i in ${computation[@]}; do
+    arr[((count++))]=$i
+done
