8f57c97a9624271062030020a035d08fb974eb82
kyoolbaba <kyoolbaba@gmail.com>
2020-04-11 22:17:50 +0530

    Added feature to store computation in descending order

--- sortAlgoComputation.sh
+++ sortAlgoComputation.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 echo Enter the three numbers separated by a space
 read A B C 
-declare -a arr
+declare -a arrAscend
+declare -a arrDescend
 computation["A+B+C"]=$((A+B+C))
 computation["A*B+C"]=$((A*B+C))
 computation["C+A/B"]=$((C+A/B))
 computation["A%B+C"]=$((A%B+C))
 for i in ${computation[@]}; do
-    arr[((count++))]=$i
+    arrAscend[((count1++))]=$i
 done
+length=${#arrAscend[@]}
+for ((i = $((length - 1)) ; i >= 0 ; i--))  do
+    arrDescend[((count2++))]=${arrAscend[i]}
+done
+ 
